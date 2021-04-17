1be5c8434fa9901b50e0413f3d3e01961bc0ebb2
getachwe <getachwe391@gmail.com>
2021-04-17 23:35:26 +0300

    new commit

--- src/Products.js
+++ src/Products.js
@@ -2,6 +2,6 @@ import './Product.css';
 import Product from './Product';
 const Products = ({ products1}) =>
 	<section className="products">
-		{props.map(p => <Product key={p.id} title={p.title} image={p.image} price={p.price}></Product>)}	
+		{products1.map(p => <Product key={p.id} title={p.title} image={p.image} price={p.price}></Product>)}	
 	</section>;
  export default Products;
